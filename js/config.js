bf491a5efd593dbc888952ce4abb93b1ea21f5a4
Sidhi goel <142575791+sidhi6276@users.noreply.github.com>
2026-02-11 17:19:35 +0000

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '',         // e.g. us-east-1_uXboG5pAb
-        userPoolClientId: '',   // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: ''              // e.g. us-east-1
+        userPoolId: 'ap-south-1_FZYpoGqOE',         // e.g. us-east-1_uXboG5pAb
+        userPoolClientId: 'uul3ps45vj28aflkinse0l5b0',   // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'ap-south-1'              // e.g. us-east-1
     },
     api: {
         invokeUrl: ''           // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod
